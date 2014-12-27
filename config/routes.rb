b04104f3af1ec536eef30a12723af55cb12f5a13
Quincy Iheme <qiheme@gmail.com>
2014-12-26 23:18:04 -0500

    switched agency stop list to agency show page

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ GothamMetro::Application.routes.draw do
 
   get 'stations', to: 'stations#index'
 
-  root 'stations#index'
+  root 'agencies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
