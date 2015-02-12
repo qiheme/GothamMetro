9fcaaf0332484c37f12a2eba5aa058783d44af6e
Quincy Iheme <qiheme@gmail.com>
2015-02-12 13:00:08 -0500

    optimized stophelper module, thanks chris

--- app/helpers/stop_helper.rb
+++ app/helpers/stop_helper.rb
@@ -1,5 +1,5 @@
 module StopHelper
   def stops
-    self.stop_times.map {|time| Stop.where(stop_id: time.stop_id)}.flatten.uniq
+    Stop.where(stop_id: self.stop_times.collect {|time| time.stop_id}.uniq)
   end
 end
