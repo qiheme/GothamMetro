59822059ba7f35a81a8ec3cf249f22677f768a2f
Quincy Iheme <qiheme@gmail.com>
2015-02-21 19:10:03 -0500

    made custom stop and trip methods

--- app/models/stop_time.rb
+++ app/models/stop_time.rb
@@ -1,4 +1,12 @@
 class StopTime < ActiveRecord::Base
 	belongs_to :stop, foreign_key: "stop_id"
 	belongs_to :trip, foreign_key: "trip_id"
+
+  def stop
+    Stop.find_by(stop_id: self.stop_id)
+  end
+
+  def trip
+    Trip.find_by(trip_id: self.trip_id)
+  end
 end
