5535297ac0aa6798600f5fb6cbcd13a72c91b8bb
Quincy Iheme <qiheme@gmail.com>
2015-08-05 07:21:12 -0400

    Remove NJT data

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -1,6 +1,2 @@
-SCHEDULE = [
-  GTFS::Source.build( File.join(Rails.root, "files", "path-nj-us.zip"), {strict: false} ),
-  GTFS::Source.build( File.join(Rails.root, "files", "rail_data.zip"), {strict: false} ),
-  GTFS::Source.build( File.join(Rails.root, "files", "bus_data.zip"), {strict: false} )
-]
+SCHEDULE = GTFS::Source.build( File.join(Rails.root, "files", "path-nj-us.zip"), {strict: false} )
 
